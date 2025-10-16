54107d10040e148904a60986445a7021636ab662
Siri <padmasiri2805@gmail.com>
2025-10-16 23:38:07 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Welcome to React App</h1>
+      <p>Deploying Sample React Vite through gitactions</p>
+    </div>
+  )
+}
+
+export default App
